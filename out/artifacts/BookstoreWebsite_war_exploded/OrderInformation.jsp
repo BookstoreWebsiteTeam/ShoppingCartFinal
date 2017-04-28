00f512bfdcee8860e4febe3e220cb4d758603fde
Adams880 <psnadams880@gmail.com>
2017-04-27 21:26:28 -0400

    Payment Info Update

--- out/artifacts/BookstoreWebsite_war_exploded/OrderInformation.jsp
+++ out/artifacts/BookstoreWebsite_war_exploded/OrderInformation.jsp
@@ -11,7 +11,7 @@
 <body>
     <table width = "75%">
         <tr>
-            <td width= "75%"><a href="http://localhost:8080/BookstoreWebsite_war_exploded/index.jsp"><img src="image/images/ksu (1).png" width="350" height="100" alt="Kennesaw Logo"></a></td>
+            <td width= "75%"><a href="${pageContext.request.contextPath}/index.jsp"><img src="image/images/ksu (1).png" width="350" height="100" alt="Kennesaw Logo"></a></td>
             <td width="25%">
                 <a href="shoppingCart.jsp"><img src="image/images/cart.png" alt="cart" width="80" height ="80"/></a>
             </td>
@@ -239,7 +239,7 @@
                     <p style="text-align: right"> Zip:</p>
                 </td>
                 <td>
-                    <input type='text' name='billingZip' value='' placeholder='Zip Code'/>
+                    <input type='text' name='billingZip' value='' placeholder='Zip Code' maxlength='5'/>
                 </td>
             </tr>
         </table>
--- src/Servlets/paymentInfoController.java
+++ src/Servlets/paymentInfoController.java
@@ -20,30 +20,36 @@ public class paymentInfoController extends HttpServlet {
         HttpSession session = request.getSession();
         String strAction = request.getParameter("paymentOptionSubmit");
         OrderInfoHandler orderInfo = (OrderInfoHandler)session.getAttribute("orderInfo");
-        String paymentSelection = request.getParameter("paymentSelect")
+        String paymentSelection = request.getParameter("paymentSelect");
         if(strAction != null && !strAction.equals("")) {
             if(paymentSelection.equals("creditCard")) {
                 String strCreditNum = request.getParameter("cardNumber");
-                int intCreditNum = Integer.parseInt(strCreditNum);
-                int creditNumLength = (int)(Math.log10(intCreditNum) + 1);
+                long longCreditNum = Long.parseLong(strCreditNum);
+                long creditNumLength = (long)(Math.log10(longCreditNum) + 1);
                 String cardName = request.getParameter("nameOnCard");
                 String cardMonth = request.getParameter("month");
                 String cardYear = request.getParameter("year");
-                int intCardYear = Integer.parseInt(cardYear)
+                int intCardYear = Integer.parseInt(cardYear);
                 String cardCvv = request.getParameter("cvv");
                 int intCardCvv = Integer.parseInt(cardCvv);
                 if(creditNumLength != 16 || intCardYear < 2017 || intCardCvv != 777) {
-                    
+                    response.sendRedirect("http://localhost:8080/BookstoreWebsite_war_exploded/paymentInfo.jsp"); //sends the user to the paymentInfo page
+                    return;
+                } else {
+                    orderInfo.setPaymentChoice("Credit Card");
+                    session.setAttribute("orderInfo", orderInfo);
+                    response.sendRedirect("http://localhost:8080/BookstoreWebsite_war_exploded/confirmation.jsp");
+                    return;
                 }
             } else if (paymentSelection.equals("paypal")) {
                 orderInfo.setPaymentChoice("Paypal");
                 session.setAttribute("orderInfo", orderInfo);
-                response.sendRedirect("http://localhost:8080/BookstoreWebsite_war_exploded/paypal.jsp"); //sends the user to the paymentInfo page
+                response.sendRedirect("http://localhost:8080/BookstoreWebsite_war_exploded/paypal.jsp"); //sends the user to the paypal page
                 return;
             } else {
                 orderInfo.setPaymentChoice("Financial Aid");
                 session.setAttribute("orderInfo", orderInfo);
-                response.sendRedirect("http://localhost:8080/BookstoreWebsite_war_exploded/login.jsp");
+                response.sendRedirect("http://localhost:8080/BookstoreWebsite_war_exploded/login.jsp"); //sends the user to the financial aid login page
             }
         }
 
